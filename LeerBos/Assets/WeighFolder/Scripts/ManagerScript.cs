8257335c3c6d0e20de241084f88bd833c692e3fc
Thomas Horvers <thorvers@xs4all.nl>
2018-10-15 16:29:34 +0200

    Game loop: game setup

--- LeerBos/Assets/WeighFolder/Scripts/ManagerScript.cs
+++ LeerBos/Assets/WeighFolder/Scripts/ManagerScript.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ManagerScript : MonoBehaviour
 {
     public GameObject WeightParent;
     public ScaleHandScript LeftHand;
-    public WeightedObjectScript ObjectToWeigh;
+    public ScaleHandScript RightHand;
+    public List<GameObject> ObjectsToWeigh;
+
+    private GameObject CurrentObject;
+    private GameState gameState;
+    private int objectsWeighed;
 
 	// Use this for initialization
 	void Start () {
-		
+		gameState = GameState.Resetting;
+	    objectsWeighed = 0;
+        CheckGameState();
 	}
 	
 	// Update is called once per frame
@@ -18,15 +27,88 @@ public class ManagerScript : MonoBehaviour
 		
 	}
 
-    //spawn the submitted prefab above the left scale
-    public void SpawnPrefab(GameObject prefab)
+    public void CheckGameState()
     {
-        Instantiate(prefab, new Vector2(GetLeftHandX(), 10),Quaternion.Euler(0,0,0),WeightParent.transform);
+        switch (gameState)
+        {
+                case GameState.Resetting:
+                    //if you've successfully weighed 5 things
+                    if (objectsWeighed >= 5)
+                    {
+                        gameState = GameState.Victory;
+                        CheckGameState();
+                    }
+                    ResetGame();
+                    gameState = GameState.Playing;
+                    break;
+                case GameState.Playing:
+                    //if the scales are equal
+                    if (LeftHand.GetTotalMass() == RightHand.GetTotalMass())
+                    {
+                        objectsWeighed++;
+                        gameState = GameState.Resetting;
+                        CheckGameState();
+                    }
+                    else
+                    {
+                        //wrong answer feedback
+                        print("wrong");
+                    }
+                    break;
+                case GameState.Victory:
+                    print("A WINNER IS YOU");
+                    break;
+        }
+    }
+
+    public void ResetGame()
+    {
+        //if not first rotation
+        if (CurrentObject != null)
+        {
+            RightHand.Objects.Remove(CurrentObject.GetComponent<WeightedObjectScript>());
+            Destroy(CurrentObject);
+        }
+        CurrentObject = ObjectsToWeigh[Random.Range(0, ObjectsToWeigh.Count)];
+        ObjectsToWeigh.Remove(CurrentObject);
+        SpawnPrefab(CurrentObject,ScaleHand.Right);
+    }
+
+    //spawn the submitted prefab above one of the scales
+    public void SpawnPrefab(GameObject prefab,ScaleHand hand)
+    {
+        float x;
+        switch (hand)
+        {
+            case ScaleHand.Left:
+                x = GetHandX(LeftHand);
+                break;
+            case ScaleHand.Right:
+                x = GetHandX(RightHand);
+                break;
+            default:
+                print("how the heck did you get here with an invalid enum");
+                throw new ArgumentException();
+        }
+        Instantiate(prefab, new Vector2(x, 10),Quaternion.Euler(0,0,0),WeightParent.transform);
     }
 
     //get x position of the left scale
-    float GetLeftHandX()
+    float GetHandX(ScaleHandScript hand)
     {
-        return LeftHand.gameObject.transform.position.x;
+        return hand.gameObject.transform.position.x;
     }
+}
+
+public enum GameState
+{
+    Resetting,
+    Playing,
+    Victory
+}
+
+public enum ScaleHand
+{
+    Left,
+    Right
 }
