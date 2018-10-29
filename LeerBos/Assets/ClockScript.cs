346095500ef5bdb889d5a0cf8e6e410e0af48139
Thomas Horvers <thorvers@xs4all.nl>
2018-10-29 15:06:10 +0100

    Clock hour hand works holy shit

--- LeerBos/Assets/ClockScript.cs
+++ LeerBos/Assets/ClockScript.cs
@@ -11,26 +11,30 @@ public class ClockScript : Interactable {
         minutesToDegrees = 360f / 60f,
         secondsToDegrees = 360f / 60f;
 
+    
     public Text TargetLabel;
 
     public Transform HourHand,MinuteHand;
 
     public int TimeSpeed;
-    private DateTime currentTime;
-    private DateTime targetTime;
-    private DateTime newTarget;
+    public float HandErrorMargin;
 
+    private TimeSpan currentTime;
+    private TimeSpan targetTime;
+    private TimeSpan newTarget;
+    private int previousMod;
+    private int previousHour;
     private bool active = true;
 
     // Use this for initialization
     void Start()
     {
         //set starting time: 12:00
-        currentTime = new DateTime(2018, 1, 1, 12, 0, 0);
+        currentTime = new TimeSpan(12, 0, 0);
         //set the first target time
         NewTargetTime();
         //set hour hand on target time in advance
-        HourHand.localRotation = Quaternion.Euler(0f, 0f, targetTime.Hour * -hoursToDegrees);
+        HourHand.localRotation = Quaternion.Euler(0f, 0f, targetTime.Hours * -hoursToDegrees);
         //start movement of minute hand
         StartCoroutine(moveHands());
         StartCoroutine(updateCurrentTime());
@@ -49,43 +53,70 @@ public class ClockScript : Interactable {
 
     void NewTargetTime()
     {
-        //get a random time modifier
-        int typeSum = Enum.GetNames(typeof(TimeModifier)).Length;
-        int rnd = UnityEngine.Random.Range(0, typeSum);
-        TimeModifier modifier = (TimeModifier)rnd;
-        //get a random hour (1-12)
-        rnd = UnityEngine.Random.Range(1, 13);
+        //get a random time
+        TimeModifier modifier = GetNewRandomMod(previousMod);
+        int hour = GetNewRandomHour(previousHour);
         //string of the written-out time to put in the label
         string targetTimeText;
         //produce the new target time
-        newTarget = ProduceTime(modifier, rnd, out targetTimeText);
+        newTarget = ProduceTime(modifier, hour, out targetTimeText);
         TargetLabel.text = targetTimeText;
         targetTime = newTarget;
-        //set hour hand on target time. animate later?
-        HourHand.localRotation = Quaternion.Euler(0f, 0f, targetTime.Hour * -hoursToDegrees);
-
-        //broken method. saved for later
-        //StartCoroutine(TransitionToTarget());
     }
 
-    DateTime ProduceTime(TimeModifier type, int hour, out string text)
+    TimeModifier GetNewRandomMod(int previous)
+    {
+        //get sum of all possible modifiers (kept dynamic for later additions)
+        int typeSum = Enum.GetNames(typeof(TimeModifier)).Length;
+        int rnd;
+        //no need to end while loop since we just return
+        while (true)
+        {
+            rnd = UnityEngine.Random.Range(0, typeSum);
+            if (rnd != previous)
+            {
+                //save to avoid repeats
+                previousMod = rnd;
+                return (TimeModifier)rnd;
+            }
+        }
+    }
+
+    int GetNewRandomHour(int previous)
+    {
+        int rnd;
+        //no need to end while loop since we just return
+        while (true)
+        {
+            rnd = UnityEngine.Random.Range(1, 13);
+            if (rnd != previous)
+            {
+                //save to avoid repeats
+                previousHour = rnd;
+                return rnd;
+            }
+        }
+    }
+
+    TimeSpan ProduceTime(TimeModifier type, int hour, out string text)
     {
         text = IntToWord(hour);
 
+        //smack that shit together, convert it to a datetime
         switch(type)
         {
             case TimeModifier.Half:
                 text = "Half " + text;
-                return new DateTime(2018, 1, 1, hour-1, 30, 0);
+                return new TimeSpan(hour-1, 30, 0);
             case TimeModifier.KwartOver:
                 text = "Kwart over " + text;
-                return new DateTime(2018, 1, 1, hour, 15, 0);
+                return new TimeSpan(hour, 15, 0);
             case TimeModifier.KwartVoor:
                 text = "Kwart voor " + text;
-                return new DateTime(2018, 1, 1, hour-1, 45, 0);
+                return new TimeSpan(hour-1, 45, 0);
             case TimeModifier.Uur:
                 text = text + " Uur";
-                return new DateTime(2018, 1, 1, hour, 0, 0);
+                return new TimeSpan(hour, 0, 0);
             default:
                 throw new ArgumentOutOfRangeException("Not a valid time modifier");
         }
@@ -94,11 +125,9 @@ public class ClockScript : Interactable {
 
     void CheckAnswer()
     {
-        int currentMinute = currentTime.Minute;
-        int targetMinute = targetTime.Minute;
-
         //margin of error of 5 minutes
-        if (targetMinute - 5 < currentMinute && currentMinute < targetMinute + 5)
+        if (targetTime.Minutes - 5 < currentTime.Minutes &&
+            currentTime.Minutes < targetTime.Minutes + 5)
         {
             print("correct!");
             //loop
@@ -119,20 +148,24 @@ public class ClockScript : Interactable {
     {
         while (active)
         {
-            //continuously turn towards the current time. will stop when reached. will move again when it changes.
-            MinuteHand.rotation = Quaternion.RotateTowards(MinuteHand.localRotation, Quaternion.Euler(0f, 0f, currentTime.Minute * -minutesToDegrees), TimeSpeed);
+            //continuously turn minute hand towards the current time. will stop when reached. will move again when it changes.
+            MinuteHand.rotation = Quaternion.RotateTowards(MinuteHand.localRotation, Quaternion.Euler(0f, 0f, currentTime.Minutes * -minutesToDegrees), TimeSpeed);
 
-            //rotating hour hand = too much work for prototype. commented for use later?
-            
-            //float target = Quaternion.Euler(0f, 0f, targetTime.Hour * -hoursToDegrees).eulerAngles.z;
+            //hour hand is more complicated
+            //can't use RotateTowards cause it'll turn counterclockwise if that's closer
+            //have to continuously rotate clockwise and stop it when close to the target. margin of error
 
-            //if (HourHand.rotation.eulerAngles.z < target + 1 && HourHand.rotation.eulerAngles.z > target -1) {
-            //    HourHand.Rotate(Vector3.back, Time.deltaTime * (TimeSpeed * 200));
-
-            //}
-            ////HourHand.rotation = Quaternion.RotateTowards(HourHand.localRotation, Quaternion.Euler(0f, 0f, targetTime.Hour * -hoursToDegrees), TimeSpeed * 2);
-
-
+            //calculate the current target Z rotation
+            float targetZ = Quaternion.Euler(0f, 0f, (float)targetTime.TotalHours * -hoursToDegrees).eulerAngles.z;
+            print(targetZ);
+            print(HourHand.eulerAngles.z);
+            //get the difference between the target and current rotation
+            float difference = HourHand.rotation.eulerAngles.z - targetZ;
+            //if not within the margin of error, keep it rotating
+            if (Mathf.Abs(difference) > HandErrorMargin)
+            {
+                HourHand.Rotate(Vector3.back, Time.deltaTime * (TimeSpeed * 200));
+            }
             yield return null;
         }
     }
@@ -147,29 +180,6 @@ public class ClockScript : Interactable {
             yield return null;
         }
     }
-
-    //finnicky coroutine for moving hour hand bit by bit. too complex for prototype. saved for later
-    //IEnumerator TransitionToTarget()
-    //{
-    //    DateTime thisTarget = newTarget;
-    //    int currentHour = targetTime.Hour;
-
-    //    while (currentHour != newTarget.Hour && thisTarget == newTarget)
-    //    {
-    //        currentHour++;
-    //        if (currentHour > 12)
-    //        {
-    //            currentHour = 1;
-    //        }
-    //        targetTime = new DateTime(2018, 1, 1, currentHour, newTarget.Minute, 0);
-
-    //        yield return new WaitForSeconds(0.5f);
-    //    }
-    //    if (thisTarget == newTarget)
-    //    {
-    //        targetTime = new DateTime(2018, 1, 1, currentHour, newTarget.Minute, 0);
-    //    }
-    //}
 
     string IntToWord(int nr)
     {
