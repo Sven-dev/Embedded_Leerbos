cb77f52399c0b9b6d2f1b7558970ad5bff47f7b8
Unknown <svenjanssen12321@gmail.com>
2018-11-07 16:51:40 +0100

    Overworld scene-switching, save file & npc-dialogue

---
+++ LeerBos/Assets/Scripts/Npc.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : Interactable
+{
+    public List<AudioClip> VoiceClips;
+    public AudioClip HitClip;
+    private AudioSource Audio;
+
+    private bool DialoguePlaying;
+    private int Index;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DialoguePlaying = false;
+
+        Audio = GetComponent<AudioSource>();
+        StartCoroutine(_PlayDialogue());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void Click(Vector3 clickposition)
+    {
+        print("Click");
+        if (DialoguePlaying)
+        {
+            print("Dialogue skip");
+            Audio.Stop();
+        }
+        else
+        {
+            print("Hit");
+            StartCoroutine(_PlayHit());
+        }
+    }
+
+    IEnumerator _PlayHit()
+    {
+        Audio.PlayOneShot(HitClip);
+        while (Audio.isPlaying)
+        {
+            yield return null;
+        }      
+    }
+
+    IEnumerator _PlayDialogue()
+    {
+        yield return new WaitForSeconds(5f);
+
+        Index = 0;
+        DialoguePlaying = true;
+        while (Index < VoiceClips.Count)
+        {
+            Audio.PlayOneShot(VoiceClips[Index]);
+            while (Audio.isPlaying)
+            {
+                yield return null;
+            }
+
+            Index++;
+            yield return new WaitForSeconds(1f);
+        }
+
+        DialoguePlaying = false;
+    }
+}
