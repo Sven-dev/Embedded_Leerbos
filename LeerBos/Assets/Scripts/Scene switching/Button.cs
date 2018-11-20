f685ea51d0d79c5a1a882f207df4175ef8196ab1
Unknown <svenjanssen12321@gmail.com>
2018-11-20 14:41:12 +0100

    Auto stash before merge of "master" and "origin/master"

--- LeerBos/Assets/Scripts/Scene switching/Button.cs
+++ LeerBos/Assets/Scripts/Scene switching/Button.cs
@@ -8,11 +8,13 @@ public class Button : SceneSwitchable
     public Sprite ImpactSprite;
     private Sprite DefaultSprite;
     private Image ButtonImage;
+    private Transform Icon;
 
     private void Start()
     {
         ButtonImage = GetComponent<Image>();
         DefaultSprite = ButtonImage.sprite;
+        Icon = transform.GetChild(0);
     }
 
     protected override void Click(Vector3 clickposition)
@@ -24,7 +26,9 @@ public class Button : SceneSwitchable
     IEnumerator _Press()
     {
         ButtonImage.sprite = ImpactSprite;
-        yield return new WaitForSeconds(0.25f);
+        Icon.localPosition = new Vector2(Icon.localPosition.x - 10, Icon.localPosition.y - 10);
+        yield return new WaitForSeconds(.5f);
+        Icon.localPosition = new Vector2(Icon.localPosition.x + 10, Icon.localPosition.y + 10);
         ButtonImage.sprite = DefaultSprite;
     }
 
