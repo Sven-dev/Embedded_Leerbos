692d96f4b917ddd1089a30325e59e26170cfd58c
Unknown <svenjanssen12321@gmail.com>
2018-12-06 11:26:29 +0100

    Bug-fixes overworld

--- LeerBos/Assets/Scripts/VariableCheckers/ZoneChecker.cs
+++ LeerBos/Assets/Scripts/VariableCheckers/ZoneChecker.cs
@@ -7,11 +7,22 @@ public class ZoneChecker : Checker
 {
     public Saveable IntroState;
     public Saveable CompleteState;
+    public List<Saveable> GameStates;
+    public GameObject[] Indicators;
+
     [Space]
     public Npc Character;
 
     protected override void Check()
     {
+        for (int i = 0; i < GameStates.Count; i++)
+        {
+            if (GameStates[i].Get() == true)
+            {
+                Destroy(Indicators[i]);
+            }
+        }
+
         if (IntroState.Get() == false)
         {
             IntroState.Set(true);
